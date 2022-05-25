33465d4a563cfe3ee88d3f01229ac665eb6de7f7
MirkOrlando <mirko@orlando-m.it>
2022-05-25 14:57:57 +0200

    create instance variables

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+
+/* create class Movie */
 class Movie
 {
+    /* initiate instance variables */
+    public $title;
+    public $image;
+    public $realise_date;
+    public $description;
 };
