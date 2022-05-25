95a05c74b8316bd6a6d90b2eb184895c08af5b82
MirkOrlando <mirko@orlando-m.it>
2022-05-25 14:58:25 +0200

    create constructor

--- index.php
+++ index.php
@@ -8,4 +8,13 @@ class Movie
     public $image;
     public $realise_date;
     public $description;
+
+    /* define constructor */
+    public function __construct($title, $image, $realise_date, $description)
+    {
+        $this->title = $title;
+        $this->image = $image;
+        $this->realise_date = $realise_date;
+        $this->description = $description;
+    }
 };
